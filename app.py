2bce8218ac487f6c0745ec67fc445495a82edc06
charling.li <charling.li@accenture.com>
2019-07-04 09:36:59 +1000

    Removed duplicate endpoint

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from scripts.flight import Flight
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Flight, "/flight/all/<int:id>")
-api.add_resource(Flight, "/flight/latest/<int:id>")
+api.add_resource(Flight, "/flight/<int:id>")
 
 if __name__ == "__main__":
   app.run()
