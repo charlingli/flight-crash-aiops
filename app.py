384d0e5ff1d32e98ddcd54d95672e1e4e2fe10f6
charling.li <charling.li@accenture.com>
2019-07-04 09:27:26 +1000

    Changed module import path for resources

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
-import resources.flight as Flight
+from resources import flight
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Flight.Flight, "/flight/all/<int:id>")
-api.add_resource(Flight.Flight, "/flight/latest/<int:id>")
+api.add_resource(Flight, "/flight/all/<int:id>")
+api.add_resource(Flight, "/flight/latest/<int:id>")
 
 if __name__ == "__main__":
   app.run()
