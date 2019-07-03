cabca95febbf77c5e67810e91b541d677cf614ca
charling.li <charling.li@accenture.com>
2019-07-04 09:13:42 +1000

    Added direct import

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), 'resources'))
-from main import Main
+
+import resources.main as Main
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Main, "/main/all/<int:id>")
-api.add_resource(Main, "/main/latest/<int:id>")
+api.add_resource(Main.Main, "/main/all/<int:id>")
+api.add_resource(Main.Main, "/main/latest/<int:id>")
 
 if __name__ == "__main__":
   app.run()
---
+++ resources/main.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource
+import os
+import sys
+import threading
+import time
+import utility
+import calculations
+
+data = []
+
+def generateLiveData():
+    startTime = time.time()
+    while True:
+        data = [ {
+            "Flight Data" : {
+              "Flight Number" : 0,
+              "Flight Metrics" : {
+                calculations.generateLiveData(utility.getFlightTime(startTime))
+              }
+            }
+        } ]
+        print(data)
+        time.sleep(5.0 - ((time.time() - startTime) % 5.0))
+
+if __name__ == "__main__":
+    t = threading.Thread(target=generateLiveData)
+    t.start()
+
+class Main(Resource):
+  def get(self, id):
+    for metric in data:
+        return metric, 200
