a5524e10d914fb5dacedaccef6caa6174bc5ea08
charling.li <charling.li@accenture.com>
2019-07-04 10:40:21 +1000

    Used external file for live data

--- scripts/flight.py
+++ scripts/flight.py
@@ -1,29 +1,11 @@
 from flask_restful import Resource
+import json
 import os
 import sys
 import threading
 import time
 import scripts.utility as utility
 import scripts.calculations as calculations
-
-global data
-data = [
-  {
-    'Flight Data': 
-    {
-      'Flight Number': 0, 
-      'Flight Metrics': 
-      {
-        'Time' : 100, 
-        'Data' : 
-        {
-          'Propulsion : Fuel' : [89.41096743],
-          'Propulsion : Engine RPM' : [2141.06811649]
-        }
-      }
-    }
-  }
-]
 
 def getData():
     startTime = time.time()
@@ -34,12 +16,15 @@ def getData():
             "Flight Metrics" : calculations.generateLiveData(utility.getFlightTime(startTime))
           }
       } ]
+      with open('liveData.txt', 'w+') as f:
+        json.dump(data, f)
       print('Data generated')
       time.sleep(5.0 - ((time.time() - startTime) % 5.0))
 
 class Flight(Resource):
   def get(self, id):
-    print(data)
+    with open('liveData.txt', 'r') as f:
+      data = json.load(f)
     return data, 200
 
 t = threading.Thread(target=getData)
