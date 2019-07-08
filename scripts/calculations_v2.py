eac5d1ad45a58504272fde65a2b0b5dc348d0b03
charlingli <charling.li@hotmail.com>
2019-07-09 07:39:41 +1000

    Fixed state equations

--- scripts/calculations_v2.py
+++ scripts/calculations_v2.py
@@ -7,15 +7,18 @@ import time
 import yaml
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-settings_file = open(dir_path + '/settings.yml', 'r')
+settings_file = open(os.path.join(dir_path, 'settings.yml'), 'r')
 settings_conf = yaml.load(settings_file, Loader=yaml.CLoader)
 settings_file.close()
+# simulation_file = dir_path + /data
 
 simulation_length = settings_conf['simulation']['length']
 simulation_multiplier = settings_conf['simulation']['multiplier']
 
 simulation_failure_type = settings_conf['failure'][np.random.randint(len(settings_conf['failure']))]
-simulation_failure_time = np.random.randint(np.floor(simulation_length / 10), np.floor(simulation_length * 9 / 10))
+simulation_failure_time = np.random.randint(np.floor(simulation_length / 5), np.floor(simulation_length * 4 / 5))
+
+print(simulation_failure_type + ' failure at ' + str(simulation_failure_time))
 
 ## Different sigmoid mixing functions to indicate different stages of flight
 data_takeoff_time = np.floor(simulation_length / 10)
@@ -24,7 +27,7 @@ data_sigmoid_takeoff = lambda flight_time: 1 / (1 + np.exp(0.1 * flight_time - (
 data_sigmoid_landing = lambda flight_time: 1 / (1 + np.exp(-0.1 * flight_time + (data_landing_time * 0.1)))
 data_sigmoid_cruise = lambda flight_time: -data_sigmoid_takeoff(flight_time) - data_sigmoid_landing(flight_time) + 1
 
-data_flight_time = np.linspace(0, simulation_length, simulation_length + 1)
+data_flight_time = np.arange(simulation_length)
 # plt.plot(data_flight_time, data_sigmoid_takeoff(data_flight_time), label=r'Takeoff sigmoid')
 # plt.plot(data_flight_time, data_sigmoid_cruise(data_flight_time), label=r'Cruise sigmoid')
 # plt.plot(data_flight_time, data_sigmoid_landing(data_flight_time), label=r'Landing sigmoid')
@@ -82,22 +85,30 @@ def calculateWeight(altitude):
 def calculateRequiredThrust(altitude, airspeed, angle_of_attack):
     return calculateDrag(altitude, airspeed, angle_of_attack) + calculateWeight(altitude) * np.sin(angle_of_attack * np.pi / 180)
 
-def calculateGeneratedThrust(altitude, airspeed, throttle_setting, flight_time):
-    if getFuelLevels(flight_time, state['fuellevels'][flight_time]) < 1: 
+def calculateGeneratedThrust(altitude, airspeed, throttle_setting, angle_of_attack, flight_time):
+    if state['fuellevels'][-1] < 1: 
         return 0
+    elif flight_time > data_takeoff_time and flight_time < data_landing_time:
+        return calculateRequiredThrust(altitude, airspeed, angle_of_attack)
     else:
         return 0.5 * data_density(altitude) * settings_conf['c172']['propellerarea'] * (np.power(settings_conf['c172']['propellervelocity'], 2) - np.power(airspeed, 2)) * throttle_setting
     
 def calculateExcessThrust(altitude, airspeed, angle_of_attack, throttle_setting, flight_time):
-    return calculateGeneratedThrust(altitude, airspeed, throttle_setting, flight_time) - calculateRequiredThrust(altitude, airspeed, angle_of_attack)
+    # print(calculateGeneratedThrust(altitude, airspeed, throttle_setting, flight_time) - calculateRequiredThrust(altitude, airspeed, angle_of_attack))
+    potential_thrust = calculateGeneratedThrust(altitude, airspeed, throttle_setting, angle_of_attack, flight_time)
+    if potential_thrust < calculateRequiredThrust(altitude, airspeed, angle_of_attack):
+        return -calculateRequiredThrust(altitude, airspeed, angle_of_attack) + 9.801
+    else:
+        return calculateGeneratedThrust(altitude, airspeed, throttle_setting, angle_of_attack, flight_time) - calculateRequiredThrust(altitude, airspeed, angle_of_attack)
 
 ## Define kinematics equations
 def calculateAccelerationX(altitude, airspeed, angle_of_attack, throttle_setting, flight_time):
-    return calculateExcessThrust(altitude, airspeed, angle_of_attack, throttle_setting, flight_time) / settings_conf['c172']['mass'] * np.cos(angle_of_attack * np.pi / 180)
+    return (calculateExcessThrust(altitude, airspeed, angle_of_attack, throttle_setting, flight_time) / settings_conf['c172']['mass'] - calculateDrag(altitude, airspeed, angle_of_attack) / settings_conf['c172']['mass']) * np.cos(angle_of_attack * np.pi / 180)
 
 def calculateAccelerationY(altitude, airspeed, angle_of_attack, throttle_setting, flight_time):
+    print(flight_time, altitude, airspeed, angle_of_attack, calculateLift(altitude, airspeed, angle_of_attack), calculateWeight(altitude), calculateExcessThrust(altitude, airspeed, angle_of_attack, throttle_setting, flight_time) / settings_conf['c172']['mass'] * np.sin(angle_of_attack * np.pi / 180))
     return calculateExcessThrust(altitude, airspeed, angle_of_attack, throttle_setting, flight_time) / settings_conf['c172']['mass'] * np.sin(angle_of_attack * np.pi / 180)
-
+    
 ## Define control equations as a function of time:
 def getPitch(flight_time):
     pitch = settings_conf['c172']['takeoff']['angleofattack'] * data_sigmoid_takeoff(flight_time) + settings_conf['c172']['cruise']['angleofattack'] * data_sigmoid_cruise(flight_time) + settings_conf['c172']['landing']['angleofattack'] * data_sigmoid_landing(flight_time);
@@ -114,7 +125,7 @@ def getPitch(flight_time):
 # plt.show()
 
 def getThrottle(flight_time):
-    throttle = settings_conf['c172']['takeoff']['throttle'] * data_sigmoid_takeoff(flight_time) + settings_conf['c172']['cruise']['throttle'] * data_sigmoid_cruise(flight_time) + settings_conf['c172']['landing']['throttle'] * data_sigmoid_landing(flight_time);
+    throttle = settings_conf['c172']['takeoff']['throttle'] * data_sigmoid_takeoff(flight_time) + settings_conf['c172']['cruise']['throttle'] * data_sigmoid_cruise(flight_time) + settings_conf['c172']['landing']['throttle'] * data_sigmoid_landing(flight_time)
     return throttle
 
 ## Define a state matrix
@@ -123,18 +134,38 @@ state = {
         "altitude": [0],
         "fuellevels": [100],
         "intakemassflowrate": [0]
-    };
+    }
 
-## Define the time-dependent equations of state
-def getAirspeed(flight_time, previous_value):
-    if (flight_time <= data_takeoff_time):
-        airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
-    elif (flight_time > data_takeoff_time) and (flight_time < data_landing_time):
-        airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
-    else:
-        airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
-    state['airspeed'].append(airspeed * np.random.normal(1, 0.001, 1))
-    return state['airspeed'][-1];
+## Define the incremental time-dependent equations of state
+def getAirspeed(flight_time):
+    potential_airspeed = calculateExcessThrust(0, state['airspeed'][-1], getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier * np.random.normal(1, 0.001, 1) + state['airspeed'][-1]
+    if state['altitude'][-1] <= 0 and flight_time > 1:
+        potential_airspeed = 0
+    state['airspeed'].append(potential_airspeed)
+    return state['airspeed'][-1]
+
+def getAltitude(flight_time):
+    potential_altitude = state['airspeed'][-1] * np.sin(getPitch(flight_time) * np.pi / 180) * np.random.normal(1, 0.002, 1) + state['altitude'][-1]
+    if potential_altitude <= 0 and flight_time > 1:
+        potential_altitude = 0
+    state['altitude'].append(potential_altitude)
+    return potential_altitude
+    
+# def getState(flight_time, previous_airspeed, previous_altitude):
+#     # if (flight_time <= data_takeoff_time):
+#     #     airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
+#     # elif (flight_time > data_takeoff_time) and (flight_time < data_landing_time):
+#     #     airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
+#     # else:
+#     #     airspeed = calculateExcessThrust(0, previous_value, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_value;
+#     # state['airspeed'].append(airspeed * np.random.normal(1, 0.001, 1))
+#     airspeed = calculateExcessThrust(0, previous_airspeed, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier + previous_airspeed
+#     altitude = calculateExcessThrust(0, previous_airspeed, getPitch(flight_time), getThrottle(flight_time), flight_time) / settings_conf['c172']['mass'] * simulation_multiplier * np.sin(getPitch(flight_time) * np.pi / 180) + previous_altitude
+#     if airspeed < 0:
+#         airspeed = 0
+#     state['airspeed'].append(airspeed * np.random.normal(1, 0.001, 1))
+#     state['altitude'].append(altitude * np.random.normal(1, 0.002, 1))
+#     return [state['airspeed'][-1], state['altitude'][-1]];
 
 # airspeed_range = np.linspace(0, 30, 30)
 # data_generated_thrust = []
@@ -154,45 +185,52 @@ def getAirspeed(flight_time, previous_value):
 # plt.legend()
 # plt.show()
 
-# plt.plot(data_flight_time, data_airspeed, label=r'Airspeed')
+## Define metrics equations
+def getFuelLevels(flight_time):
+    if simulation_failure_type == 'fuellevels' and flight_time > simulation_failure_time:
+        fuel_function = state['fuellevels'][-1] - 0.4 if state['fuellevels'][-1] > 1 else 0
+    else:
+        fuel_function = state['fuellevels'][-1] - getThrottle(flight_time) * settings_conf['c172']['fuelefficiency'] * np.random.poisson(1, 1)
+    state['fuellevels'].append(fuel_function)
+    return state['fuellevels'][-1]
+
+for i in data_flight_time[:-1]:
+    (getAirspeed(i))
+    (getAltitude(i))
+
+print(len(state['airspeed']))
+
+# plt.plot(data_flight_time, state['airspeed'], label=r'Airspeed')
 # plt.xlabel(r'Flight Time (s)')
 # plt.ylabel(r'Airspeed (m/s)')
 # plt.title(r'Airspeed')
 # plt.show()
 
-def getAltitude(flight_time, previous_value):
-    altitude = calculateAccelerationY(previous_value, getAirspeed(flight_time, previous_value), getPitch(flight_time), getThrottle(flight_time), flight_time) + previous_value
-    state['altitude'].append(altitude * np.random.normal(1, 0.002, 1))
-    return state['altitude'][-1];
+plt.plot(data_flight_time, state['altitude'], label=r'Altitude')
+plt.xlabel(r'Flight Time (s)')
+plt.ylabel(r'Altitude (m)')
+plt.title(r'Altitude')
+plt.show()
 
-# data_altitude = [0]
-# for i in data_flight_time[1:]:
-#     data_altitude.append(getAltitude(i, data_altitude[-1]))
+# print(state['fuellevels'])
 
-# plt.plot(data_flight_time, data_altitude, label=r'Altitude')
+# plt.plot(data_flight_time, np.cumsum(data_thrust), label=r'Altitude')
 # plt.xlabel(r'Flight Time (s)')
 # plt.ylabel(r'Altitude (m)')
 # plt.title(r'Altitude')
 # plt.show()
 
-## Define metrics equations
-def getFuelLevels(flight_time, previous_value):
-    if simulation_failure_type == 'fuellevels' and flight_time > simulation_failure_time:
-        fuel_function = previous_value - 0.4 if previous_value > 1 else 0
-    else:
-        fuel_function = previous_value - getThrottle(flight_time) * settings_conf['c172']['fuelefficiency'] * np.random.poisson(1, 1)
-    state['fuellevels'].append(fuel_function)
-    return state['fuellevels'][-1]
+# print(np.cumsum(data_altitude))
 
-fuel_levels = [100]
-for i in data_flight_time[1:]:
-    fuel_levels.append(getFuelLevels(i, fuel_levels[-1]))
+# fuel_levels = [100]
+# for i in data_flight_time[1:]:
+#     fuel_levels.append(getFuelLevels(i, fuel_levels[-1]))
 
-# plt.plot(data_flight_time, fuel_levels, label=r'Fuel Levels')
-# plt.xlabel(r'Flight Time (s)')
-# plt.ylabel(r'Fuel Levels (%)')
-# plt.title(r'Fuel Level')
-# plt.show()
+plt.plot(data_flight_time, state['fuellevels'], label=r'Fuel Levels')
+plt.xlabel(r'Flight Time (s)')
+plt.ylabel(r'Fuel Levels (%)')
+plt.title(r'Fuel Level')
+plt.show()
 
 def getIntakeMassFlowRate(flight_time, previous_value):
     if simulation_failure_type == 'intakemassflowrate' and flight_time > simulation_failure_time:
@@ -202,17 +240,15 @@ def getIntakeMassFlowRate(flight_time, previous_value):
     state['intakemassflowrate'].append(intake_function)
     return state['intakemassflowrate'][-1]
 
-fuel_levels = [100]
-for i in data_flight_time[1:]:
-    fuel_levels.append(getFuelLevels(i, fuel_levels[-1]))
+# fuel_levels = [100]
+# for i in data_flight_time[1:]:
+#     fuel_levels.append(getFuelLevels(i, fuel_levels[-1]))
 
-plt.plot(data_flight_time, fuel_levels, label=r'Fuel Levels')
-plt.xlabel(r'Flight Time (s)')
-plt.ylabel(r'Fuel Levels (%)')
-plt.title(r'Fuel Level')
-plt.show()
-
-print(simulation_failure_type + ' failure at ' + str(simulation_failure_time))
+# plt.plot(data_flight_time, fuel_levels, label=r'Fuel Levels')
+# plt.xlabel(r'Flight Time (s)')
+# plt.ylabel(r'Fuel Levels (%)')
+# plt.title(r'Fuel Level')
+# plt.show()
 
 # print(calculateLift(2, 27.778, 5.768))
 # print(calculateWeight(2))
@@ -224,6 +260,14 @@ print(simulation_failure_type + ' failure at ' + str(simulation_failure_time))
 # print(calculateVelocityX(2, 27.778, 5.768, 0.362))
 # print(calculateVelocityY(2, 27.778, 5.768, 0.362))
 
-# print(simulation_failure_type + ' failure at ' + str(simulation_failure_time))
+def getLiveState(state, flight_time):
+    getAirspeed(flight_time, state['airspeed'][-1])
+    getAltitude(flight_time, state['altitude'][-1])
+    getFuelLevels(flight_time, state['fuellevels'][-1])
+    getIntakeMassFlowRate(flight_time, state['intakemassflowrate'][-1])
+    return state
 
-# def getLiveState():
+# print(data_flight_time)
+
+# for i in data_flight_time:
+#     print(getAirspeed(int(i), state['airspeed'][-1]))
